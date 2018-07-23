acbb0f90c16679cde170f59abbbb767f9919f91e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 13:55:51 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,13 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS students (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  grade TEXT
+)
+SQL
+ActiveRecord::Base.execute(sql)
+
+
 end
